97a15e24cb6ccfa9a7280c2bd821c488314b711a
Octowl <alsaff1987@gmail.com>
2019-02-04 17:34:40 +0300

    cleanup forms

--- src/forms/BookForm.js
+++ src/forms/BookForm.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-
-import bookStore from "../stores/BookStore";
 import { observer } from "mobx-react";
+
+import bookStore from "../stores/bookStore";
 
 class BookForm extends Component {
   constructor(props) {
@@ -26,26 +26,41 @@ class BookForm extends Component {
 
   render() {
     return (
-      <form onSubmit={this.submitBook}>
-        <input
-          type="text"
-          name="title"
-          placeholder="Book Name"
-          onChange={this.onTextChange}
-        />
-        <select name="color" onChange={this.onTextChange}>
-          <option value="">Color</option>
-          <option value="red">Red</option>
-          <option value="blue">Blue</option>
-          <option value="green">Green</option>
-          <option value="yellow">Yellow</option>
-          <option value="black">Black</option>
-          <option value="white">White</option>
-          <option value="grey">Grey</option>
-          <option value="purple">Purple</option>
-        </select>
-        <input type="submit" value="Add Book" />
-      </form>
+      <div className="mt-5">
+        <form onSubmit={this.submitBook}>
+          <div className="input-group mb-3">
+            <div className="input-group-prepend">
+              <span className="input-group-text">Title</span>
+            </div>
+            <input
+              type="text"
+              className="form-control"
+              name="title"
+              onChange={this.onTextChange}
+            />
+          </div>
+          <div className="input-group mb-3">
+            <div className="input-group-prepend">
+              <span className="input-group-text">Color</span>
+            </div>
+            <select
+              name="color"
+              className="form-control"
+              onChange={this.onTextChange}
+            >
+              <option value="white">White</option>
+              <option value="red">Red</option>
+              <option value="blue">Blue</option>
+              <option value="green">Green</option>
+              <option value="yellow">Yellow</option>
+              <option value="black">Black</option>
+              <option value="grey">Grey</option>
+              <option value="purple">Purple</option>
+            </select>
+          </div>
+          <input type="submit" value="Add Book" />
+        </form>
+      </div>
     );
   }
 }
